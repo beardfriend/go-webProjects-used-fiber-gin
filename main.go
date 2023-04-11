71e048768fb576fe72d3202402a49a2e5f76d3e7
beardfriend <beardfriend21@gmail.com>
2023-04-11 21:27:26 +0900

    refactoring

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -13,63 +12,126 @@ import (
 )
 
 func main() {
-	var star int
-	var url string
+	var results []string
+	var i int
+	var str string
+	for {
+		c := NewCollector()
+		c.setStarred(10)
 
-	flag.StringVar(&url, "url", "", "Enter package name e.g. gocolly/colly")
-	flag.IntVar(&star, "star", 0, "Enter min star for crawl")
-	flag.Parse()
+		if i == 0 {
+			c.setStartUrl("https://github.com/gin-gonic/gin/network/dependents")
+		} else {
+			c = c.setStartUrl(str)
+		}
 
-	if url == "" || star == 0 {
-		fmt.Println("error")
-		os.Exit(1)
+		str = c.Start()
+		results = append(results, c.results...)
+		i++
+
+		if str == "" {
+			break
+		}
+
 	}
-	c := colly.NewCollector()
+}
 
-	var nextPageLinks []string
-	var isFinish bool
+// github opensource used page collector
+type Collector struct {
+	engine *colly.Collector
 
-	c.OnHTML(".paginate-container .BtnGroup a.BtnGroup-item:last-child", func(e *colly.HTMLElement) {
+	startUrl string
+
+	nextPageLinks []string
+
+	isFinish bool
+
+	starred int
+
+	results []string
+
+	// for error
+	sleepingSecond int
+}
+
+func NewCollector() *Collector {
+	return &Collector{
+		engine:         colly.NewCollector(),
+		sleepingSecond: 5,
+	}
+}
+
+func (c *Collector) Start() string {
+	c.engine.Init()
+	c.getNextLink()
+	c.getProjects()
+	c.retryEngine()
+	c.engine.Visit(c.startUrl)
+
+	for !c.isFinish {
+		nextLink := c.nextPageLinks[len(c.nextPageLinks)-1]
+		c.engine.Visit(nextLink)
+	}
+
+	return c.nextPageLinks[len(c.nextPageLinks)-1]
+}
+
+func (c *Collector) getNextLink() {
+	c.engine.OnHTML(".paginate-container .BtnGroup a.BtnGroup-item:last-child", func(e *colly.HTMLElement) {
 		nextLink := e.Attr("href")
-		nextPageLinks = append(nextPageLinks, nextLink)
+		c.nextPageLinks = append(c.nextPageLinks, nextLink)
 		if strings.Contains(e.Attr("disabled"), "disabled") {
-			isFinish = true
+			c.nextPageLinks = append(c.nextPageLinks, "")
+			c.isFinish = true
 		}
 	})
+}
 
-	c.OnHTML(".application-main .Box", func(e *colly.HTMLElement) {
+func (c *Collector) getProjects() {
+	c.engine.OnHTML(".application-main .Box", func(e *colly.HTMLElement) {
 		e.ForEach(".Box-row", func(i int, h *colly.HTMLElement) {
 			text := h.ChildText(".d-flex span:first-child")
 			star, _ := strconv.Atoi(text)
-			if star >= 1 {
+			if star >= c.starred {
 				url := h.ChildAttr(".f5 a.text-bold", "href")
-				fmt.Println(url)
+				c.results = append(c.results, url)
+				fmt.Println(c.results)
 			}
 		})
 	})
+}
 
-	c.OnError(func(r *colly.Response, err error) {
-		if r.StatusCode == 429 {
+func (c *Collector) retryEngine() {
+	c.engine.OnError(func(r *colly.Response, err error) {
+		if r.StatusCode != 200 {
 			log.Println("Received 429 response. Retrying after 5 seconds...")
-			time.Sleep(5 * time.Second)
-			c.Visit(r.Request.URL.String())
+			time.Sleep(time.Duration(c.sleepingSecond) * time.Second)
+			c.nextPageLinks = append(c.nextPageLinks, r.Request.URL.String())
+			c.isFinish = true
 		} else {
 			log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 		}
 	})
+}
 
-	err := c.Visit("https://github.com/gin-gonic/gin/network/dependents")
-	if err != nil {
-		log.Fatal(err)
+func (c *Collector) setStartUrl(url string) *Collector {
+	c.startUrl = url
+	return c
+}
+
+func (c *Collector) setStarred(starred int) *Collector {
+	c.starred = starred
+	return c
+}
+
+func (c *Collector) checkStartPossible() error {
+	if c.startUrl == "" {
+		return errors.New("please set Url")
 	}
 
-	for !isFinish {
-		nextLink := nextPageLinks[len(nextPageLinks)-1]
-		err := c.Visit(nextLink)
-		if err != nil {
-			log.Fatal(err)
-		}
+	if c.starred == 0 {
+		return errors.New("please set starred")
 	}
 
-	c.Wait()
+	return nil
 }
